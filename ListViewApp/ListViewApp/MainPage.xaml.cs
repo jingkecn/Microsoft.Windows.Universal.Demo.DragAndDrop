54e71fdc14282456f3f595059fe5838ecb7e3baa
Ke Jing <jing.ke@myscript.com>
2018-02-23 17:15:29 +0100

    Initial project: ListViewApp.

---
+++ ListViewApp/ListViewApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListViewApp
+{
+    /// <inheritdoc cref="Page" />
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
